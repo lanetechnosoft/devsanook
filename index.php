ea100d3ad1524ede2917b6e66b735bad00038286
Administrator@Lane <hdbdudbsjwjs@gmail.com>
2020-04-02 12:01:03 +0700

    make it better

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
-require_once('./configs/db.php');
+
 header( "Content-type:application/json; charset=UTF-8" );
+header("Cache-Control: no-store, no-cache, must-revalidate");         
+header("Cache-Control: post-check=0, pre-check=0", false);
+require_once('./configs/db.php');
 try {
     $json_data = array();
 $obj = new myPDO();
